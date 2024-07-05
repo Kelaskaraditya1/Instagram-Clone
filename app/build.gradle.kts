c1ea48c2a0ae821383bdc3c3bb82d3ed4b4c999d
Aditya S Kelaskar <kelaskaraditya1@gmail.com>
2024-07-06 04:07:14 +0530

    Signin/Up Ui done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -39,12 +40,14 @@ android {
 }
 
 dependencies {
-
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+    implementation("com.google.android.material:compose-theme-adapter:1.2.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
